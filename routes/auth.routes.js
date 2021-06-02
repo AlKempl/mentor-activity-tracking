23217288250dd58d078d9a81df72b939babc1fdf
alex <alkempled@gmail.com>
2021-06-03 00:51:20 +0300

    Added API validators

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,18 +1,27 @@
 let express = require('express');
 let router = express.Router();
 
-const { verifySignUp } = require("../middleware");
+const {verifySignUp} = require("../middleware");
 const controller = require("../controllers/auth.controller");
+const {body, validationResult} = require('express-validator')
+
 
 router.post(
     "/signup",
     [
         verifySignUp.checkDuplicateUsernameOrEmail,
-        verifySignUp.checkRolesExisted
+        verifySignUp.checkRolesExisted,
+        body('username').trim().notEmpty().isAlpha('en-US').isLength({min: 3}).escape(),
+        body('email').normalizeEmail().isEmail(),
+        body('password').trim().notEmpty().escape()
     ],
     controller.signup
 );
 
-router.post("/signin", controller.signin);
+router.post("/signin", [
+            body('username').trim().notEmpty().isAlpha('en-US').isLength({min: 3}).escape(),
+            body('password').trim().notEmpty().escape()
+    ],
+    controller.signin);
 
 module.exports = router;
