8d827b421043f4ba5b0efaa5ef148a31cf31e3a7
alex <alkempled@gmail.com>
2021-06-03 07:52:33 +0300

    Darned DWH Home!

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -33,7 +33,7 @@ export default class Home extends Component {
         return (
             <div className="container">
                 <header className="jumbotron">
-                    <h3>{this.state.content}</h3>
+                    <img src="https://www.meme-arsenal.com/memes/4f25111141e51434c4d1bb4277e804c1.jpg" alt="alt"/>
                 </header>
             </div>
         );
