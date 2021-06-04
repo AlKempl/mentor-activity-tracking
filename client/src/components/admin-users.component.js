315033ecdc58a50b3c8e444bbeb15e7512e30fd0
alex <alkempled@gmail.com>
2021-06-04 23:13:26 +0300

    Admin users list

--- client/src/components/admin-users.component.js
+++ client/src/components/admin-users.component.js
@@ -4,46 +4,46 @@ import AdminUsersService from "../services/admin.users.service";
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import Jumbotron from "react-bootstrap/Jumbotron";
 import UITable from "./ui/UITable";
+import Spinner from "react-bootstrap/Spinner";
 
 export default class AdminUsersComponent extends Component {
     constructor(props) {
         super(props);
 
-        this.columns = [{
+        let columns = [{
             dataField: 'id',
-            text: 'User ID'
+            text: 'User ID',
+            width: null
         }, {
             dataField: 'username',
-            text: 'Username'
+            text: 'Username',
+            width: null
         }, {
             dataField: 'createdAt',
-            text: 'Created At'
+            text: 'Created At',
+            width: null
         }, {
             dataField: 'updatedAt',
             text: 'Updated At',
-            editor: null
+            width: null
+        }, {
+            dataField: 'enabled',
+            text: 'Enabled',
+            width: null
+        }];
+
+        let actions = {
+            create: {formPath:'/admin'},
+            update: {},
+            delete: {},
+            switch: {}
         }
-            // , {
-            //     dataField: 'roles[].name',
-            //     text: 'Roles'
-            // }
-            , {
-                dataField: 'enabled',
-                text: 'Enabled',
-                editor: {
-                    type: null,
-                    options: [{
-                        value: '0',
-                        label: 'Disabled'
-                    }, {
-                        value: '1',
-                        label: 'Enabled'
-                    }]
-                }
-            }];
 
         this.state = {
             users_content: [],
+            columns: columns,
+            isLoading: true,
+            actions: actions
         };
     }
 
@@ -51,7 +51,8 @@ export default class AdminUsersComponent extends Component {
         await AdminUsersService.list().then(
             response => {
                 this.setState({
-                    users_content: response.data.users
+                    users_content: response.data.users,
+                    isLoading: false
                 });
             },
             error => {
@@ -61,7 +62,8 @@ export default class AdminUsersComponent extends Component {
                             error.response.data &&
                             error.response.data.message) ||
                         error.message ||
-                        error.toString()
+                        error.toString(),
+                    isLoading: false
                 });
             }
         );
@@ -70,7 +72,18 @@ export default class AdminUsersComponent extends Component {
     render() {
         return (
             <Jumbotron>
-                <UITable data={this.state.users_content}/>
+                {
+                    this.state.isLoading ?
+                        <div>
+                            <Spinner animation="border"/>
+                        </div>
+                        :
+                        <UITable
+                            columns={this.state.columns}
+                            data={this.state.users_content}
+                            actions={this.state.actions}
+                        />
+                }
             </Jumbotron>
         );
     }
