aceb2719c00b34e52ddeccc2c337fc78303ba2a8
alex <alkempled@gmail.com>
2021-06-04 20:23:10 +0300

    Profile refactor

--- client/src/components/profile.component.js
+++ client/src/components/profile.component.js
@@ -4,8 +4,12 @@ import "./login.component.css";
 import AuthService from "../services/auth.service";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
+import {CopyToClipboard} from "react-copy-to-clipboard";
+
+require('dotenv').config()
 
 export default class Profile extends Component {
 
@@ -22,33 +26,69 @@ export default class Profile extends Component {
 
         return (
             <Jumbotron>
-                <Card style={{width: '30rem'}}>
-                    <Card.Header>
-                        <h3><strong>{currentUser.username}</strong> Profile</h3>
-                    </Card.Header>
-                    <Card.Body style={{textAlign: "left"}}>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem>
-                                <strong>Token:</strong>{" "}
-                                {currentUser.accessToken.substring(0, 6)} **** {" "}
-                                {currentUser.accessToken.substr(currentUser.accessToken.length - 6)}
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <strong>Id:</strong>{" "}
-                                {currentUser.id}
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <strong>Email:</strong>{" "}
-                                {currentUser.email}
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <strong>Authorities: </strong>
-                                {currentUser.roles &&
-                                currentUser.roles.join(', ')}
-                            </ListGroupItem>
-                        </ListGroup>
-                    </Card.Body>
-                </Card>
+                <CardDeck>
+                    <Card style={{width: '30rem'}}>
+                        <Card.Header>
+                            <h3><strong>Security</strong></h3>
+                        </Card.Header>
+                        <Card.Body style={{textAlign: "left"}}>
+                            <ListGroup className="list-group-flush">
+                                <ListGroupItem>
+                                    <strong>Environment: </strong>
+                                    {
+                                        ['production', 'staging'].includes(process.env.NODE_ENV) ?
+                                            'Production' :
+                                            'Development'
+                                    }
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    <strong>Token: </strong>{
+                                    ['production', 'staging'].includes(process.env.NODE_ENV) ?
+                                        currentUser.accessToken.substring(0, 6)
+                                        + " **** " +
+                                        currentUser.accessToken.substr(currentUser.accessToken.length - 6)
+                                        :
+                                        <CopyToClipboard text={currentUser.accessToken}
+                                                         onCopy={() => this.setState({copied: true})}>
+                                            <div>{currentUser.accessToken}</div>
+                                        </CopyToClipboard>
+                                }
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    <strong>Id:</strong>{" "}
+                                    {currentUser.id}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    <strong>Authorities: </strong>
+                                    {currentUser.roles &&
+                                    currentUser.roles.join(', ')}
+                                </ListGroupItem>
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{width: '30rem'}}>
+                        <Card.Header>
+                            <h3><strong>{currentUser.username}</strong> Profile</h3>
+                        </Card.Header>
+                        <Card.Body style={{textAlign: "left"}}>
+                            <ListGroup className="list-group-flush">
+                                <ListGroupItem>
+                                    <strong>Username:</strong>{" "}
+                                    {currentUser.username}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    <strong>Email:</strong>{" "}
+                                    {currentUser.email}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    <strong>Authorities: </strong>
+                                    {currentUser.roles &&
+                                    currentUser.roles.join(', ')}
+                                </ListGroupItem>
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
+                </CardDeck>
             </Jumbotron>
         );
     }
